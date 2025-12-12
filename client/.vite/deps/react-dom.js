dd32c7f7e47252b658ec64a93f3775eff5ca882b
Veniamin <benbrykin@gmail.com>
2025-12-12 14:36:18 +0300

    Refactor user data handling: streamline initialization process and improve server response

---
+++ client/.vite/deps/react-dom.js
@@ -0,0 +1,6 @@
+import {
+  require_react_dom
+} from "./chunk-ADAXAUTE.js";
+import "./chunk-HNZ3AJVS.js";
+import "./chunk-G3PMV62Z.js";
+export default require_react_dom();
---
+++ node_modules/credit-terminator-client/.vite/deps/react.js
@@ -0,0 +1,5 @@
+import {
+  require_react
+} from "./chunk-HNZ3AJVS.js";
+import "./chunk-G3PMV62Z.js";
+export default require_react();
